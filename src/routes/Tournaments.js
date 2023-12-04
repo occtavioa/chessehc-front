97fc6805db92599ab562dd2ed647bec86bb3690b
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-12-04 10:32:50 -0300

    style

--- src/routes/Tournaments.js
+++ src/routes/Tournaments.js
@@ -1,13 +1,22 @@
+import { Alert, Button, Container } from "react-bootstrap"
 import { useLoaderData } from "react-router-dom"
 import TournamentsList from "../components/TournamentsList"
 
 function Tournaments() {
     const tournaments = useLoaderData()
 
+    console.log("tournaments:", tournaments);
+    
     return (
-        <>
-            <TournamentsList tournaments={tournaments} />
-        </>
+        <Container>
+            {
+                tournaments.length === 0
+                ? <Alert variant="light">No tournaments found</Alert>
+                : <>
+                    <TournamentsList tournaments={tournaments}/>
+                </>
+            }
+        </Container>
     )
 }
 
