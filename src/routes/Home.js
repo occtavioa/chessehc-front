b7710762cf4ec04bd96c71ce4bb13b6f3692ea1f
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-12-04 10:31:52 -0300

    show 6 most recent tournaments

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,21 @@
+import { Alert, Container, ListGroup } from "react-bootstrap"
+import { Link, useLoaderData } from "react-router-dom"
+import TournamentsList from "../components/TournamentsList"
+
 function Home() {
+    const tournaments = useLoaderData()
+
     return (
-        <>Home</>
+        <Container>
+            {
+                tournaments.length === 0
+                ? <Alert variant="light">No tournaments found</Alert>
+                : <>
+                    <Alert variant="info">Most recent tournaments. <Alert.Link as={Link} to="/tournaments">See all</Alert.Link></Alert>
+                    <TournamentsList tournaments={tournaments}/>
+                </>
+            }
+        </Container>
     )
 }
 
